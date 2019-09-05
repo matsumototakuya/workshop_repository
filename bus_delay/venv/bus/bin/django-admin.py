e8d9ceec7244232249d1f8e3bc8979e3b9a64914
Takuya Matsumoto <>
2019-09-05 20:34:29 +0900

    仮想環境

---
+++ bus_delay/venv/bus/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/takuyamatsumoto/workshop_repository/bus_delay/venv/bus/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
