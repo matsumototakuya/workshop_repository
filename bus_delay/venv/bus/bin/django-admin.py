aed81bfcff76baee3da4d78d8fc62da998e85a4e
Takuya Matsumoto <>
2019-09-07 15:11:41 +0900

    no message

--- bus_delay/venv/bus/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/takuyamatsumoto/workshop_repository/bus_delay/venv/bus/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
