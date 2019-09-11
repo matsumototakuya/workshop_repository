904cbc6d6db8ae5af7c4d52a085cc89b05648997
Takuya Matsumoto <>
2019-09-12 01:12:00 +0900

    入力したデータを用いてモデルの更新ができるようにしました．

--- delay_predict/app.py
+++ delay_predict/app.py
@@ -1,25 +1,38 @@
 from flask import Flask, render_template, request, flash
-from wtforms import Form, FloatField, SubmitField, validators, ValidationError
+from wtforms import Form, FloatField, IntegerField, SubmitField, validators, ValidationError
 import numpy as np
 from sklearn.externals import joblib
-
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor as RFR
 
 # 学習済みモデルを読み込み利用します
 def predict(parameters):
-    # ニューラルネットワークのモデルを読み込み
+    # モデルを読み込み
     model = joblib.load('./rf.pkl')
     params = parameters.reshape(1, -1)
     pred = model.predict(params)
     return pred
 
+def update(parameters, label):
+    # データ取得
+    data = np.load(file ="data.npy")
+    target = np.load(file ="target.npy")
+    x = np.append(data,[parameters],axis=0)
+    y= np.append(target,[label],axis=0)
+    # 訓練データとテストデータに分割
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+    model = RFR(n_jobs=1, random_state=0, n_estimators=5)
+    # 学習
+    model.fit(x_train, y_train)
+    # 学習済みモデル，データセットの保存
+    joblib.dump(model, "rf.pkl", compress=True)
+    np.save("data", x)
+    np.save("target", y)
+    score = model.score(x_test, y_test)
+    return score
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = 'zJe09C5c3tMf5FnNL09C5d6SAzZoY'
 
-
-# 公式サイト
-# http://wtforms.simplecodes.com/docs/0.6/fields.html
 # Flaskとwtformsを使い、index.html側で表示させるフォームを構築します。
 class DelayForm(Form):
     Time = FloatField("時刻(時)",
@@ -41,6 +54,12 @@ class DelayForm(Form):
     # html側で表示するsubmitボタンの表示
     submit = SubmitField("予測")
 
+class LabelForm(Form):
+    Label = IntegerField("遅延時間(分)",
+                         [validators.InputRequired("この項目は入力必須です"),
+                          validators.NumberRange(min=0, max=10)])
+    submit = SubmitField("更新")
+
 
 @app.route('/', methods=['GET', 'POST'])
 def predicts():
@@ -57,11 +76,28 @@ def predicts():
 
             x = np.array([Time, Traffic, Passenger, Rainy])
             pred = predict(x)
+            np.save("parameters", x)
 
             return render_template('result.html', pred=round(pred[0],1))
     elif request.method == 'GET':
 
         return render_template('index.html', form=form)
+
+@app.route('/update/', methods = ['GET', 'POST'])
+def updates():
+    form = LabelForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash("全て入力する必要があります。")
+            return render_template('update.html', form=form)
+        else:
+            Label = int(request.form["Label"])
+            Parameters = np.load(file ="parameters.npy")
+            score = update(Parameters, Label)
+
+            return render_template('finish.html', score=round(score, 3))
+    elif request.method == 'GET':
+        return render_template('update.html', form=form)
 
 
 if __name__ == "__main__":
--- delay_predict/init_data.py
+++ delay_predict/init_data.py
@@ -1,16 +1,16 @@
 # ライブラリのimport
 import pandas as pd
+import numpy as np
 from sklearn.ensemble import RandomForestRegressor as RFR
 from sklearn.model_selection import train_test_split
 from sklearn.externals import joblib
-from sklearn.metrics import classification_report, accuracy_score
 
 # データの読み込み
 dataset = pd.read_excel('basutienn.xlsx')
 
 # 教師データとテストデータに分割
 train_data, test_data, train_target, test_target = train_test_split(dataset.iloc[:, 1:5], dataset.iloc[:, 5],
-                                                                    test_size=0.2, random_state=0)
+                                                                    test_size=0.3, random_state=0)
 
 rg = RFR(n_jobs=1, random_state=0, n_estimators=5)  # randomforest
 
@@ -22,3 +22,9 @@ joblib.dump(rg, "rf.pkl", compress=True)
 
 # 予測精度
 print("result: ", rg.score(test_data, test_target))
+
+# データの保存
+data = dataset.iloc[:, 1:5].values
+target = dataset.iloc[:, 5].values
+np.save("data", data)
+np.save("target", target)
